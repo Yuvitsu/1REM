9e12e840efc324901759ef22c1a6a8b9a3992239
Yuvitsu <text.demo.script@gmail.com>
2025-02-06 17:08:22 +0900

    エポックを１００に修正

--- lstm_model.py
+++ lstm_model.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     lstm_model.fit(
         train_dataset,
         validation_data=val_dataset,
-        epochs=1,
+        epochs=100,
         callbacks=[loss_logger]
     )
 
