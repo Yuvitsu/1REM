444230e90feeaf2786b26b5ac774fdb9492b1f0c
Yuvitsu <text.demo.script@gmail.com>
2025-03-07 14:25:02 +0900

    gym

--- lstm_model.py
+++ lstm_model.py
@@ -57,7 +57,7 @@ def build_lstm(input_shape, learning_rate):
 # --- メイン処理 ---
 if __name__ == "__main__":
     print("=== データの作成を開始 ===")
-    data_loader = DataLoader(data_dir="Data_Label/E420")
+    data_loader = DataLoader(data_dir="Data_Label/Gym")
     x_data, y_label = data_loader.load_data()
 
     batch_size = 64
